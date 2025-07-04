42a09de487eed30e534e327809a1df7f4351851f
vickii20 <vigneshramamoorthy04@gmail.com>
2025-07-04 22:00:44 +0530

    new prg

---
+++ if-statement/positive_negative_or_zero.py
@@ -0,0 +1,8 @@
+numbers = int(input("Enter the Numbers: "))
+
+if numbers >= 1:
+    print("Positive Number")
+elif numbers <= -1:
+    print("Negative Number")
+elif numbers == 0:
+    print("Zero")
